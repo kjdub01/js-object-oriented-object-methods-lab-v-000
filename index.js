ee714f013da3826466ca3f2f6628d70a45c95a4b
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-09 03:45:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function BoardMember(name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
+  
   this.veto = function () {
     return 'No, I must disagree';
   }
+  
   this.approve = function () {
     return 'You can do that!'
   }
